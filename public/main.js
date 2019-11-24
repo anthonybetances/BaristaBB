2fad49c2f306f39bc3f7687806b3f7415b1520a5
Betances <tonybetancesdev@gmail.com>
2019-11-24 18:07:51 -0500

    First Commit

---
+++ public/main.js
@@ -0,0 +1,47 @@
+var trash = document.getElementsByClassName("fa-trash");
+let coffeeDone = document.getElementsByClassName("fa-edit")
+let coffeeSubmit = document.getElementsByClassName("coffeeSubmit")
+
+Array.from(coffeeDone).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const  cust = this.parentNode.parentNode.childNodes[3].innerText
+        const order = this.parentNode.parentNode.childNodes[7].innerText
+        fetch('coffeePlace', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            'cust':cust,
+            'order':order,
+          })
+        })
+        .then(response => {
+          if (response.ok) return response.json()
+        })
+        .then(data => {
+          console.log(data)
+          window.location.reload(true)
+        })
+      });
+});
+
+
+Array.from(trash).forEach(function(element) {
+      element.addEventListener('click', function(){
+        const cust = this.parentNode.parentNode.childNodes[3].innerText
+        console.log('cust', cust)
+        const order = this.parentNode.parentNode.childNodes[7].innerText
+        console.log('order',order)
+        fetch('coffeePlace', {
+          method: 'delete',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+            'cust': cust,
+            'order': order
+            })
+        }).then(function (response) {
+          window.location.reload()
+        })
+      });
+});
